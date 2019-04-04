d43d4c68884b544fcf433454cb9f91be2e297443
Eduvie Agada <eduvieagada@gmail.com>
2019-04-05 00:03:16 +0100

    started development

---
+++ src/product/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+const Product = (props) => (
+    <div className={'product'}>
+        <p>Test Product</p>
+    </div>
+);
+
+export default Product;
